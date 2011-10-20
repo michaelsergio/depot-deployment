ff4fc9698dbc47bf49cff1df33c6c8f68bb887d5
Mike Sergio <mikeserg@gmail.com>
2011-10-20 01:07:50 -0400

    cart creation complete

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
+    @products = Product.all
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
   end
 
 end
